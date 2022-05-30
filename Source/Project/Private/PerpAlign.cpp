b1d758458abb4f9001e39b6b16adae586b639587
KiSae-Lee <kisaelee.github@gmail.com>
2022-05-30 17:31:55 +0900

    Add debug message func.

--- Source/Project/Private/PerpAlign.cpp
+++ Source/Project/Private/PerpAlign.cpp
@@ -8,14 +8,18 @@ APerpAlign::APerpAlign()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	
 }
 
 // Called when the game starts or when spawned
 void APerpAlign::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if(!CurveFloat)
+	{
+		
+	}
 }
 
 // Called every frame
@@ -24,6 +28,15 @@ void APerpAlign::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
+
+void APerpAlign::DebugMessage(FString Message, FColor Color, float Delay)
+{
+	if (IsDebug)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, Delay, Color, Message);
+	}
+}
+
 
 // Timeline functions from here.
 void APerpAlign::InProgress()
--- Source/Project/Public/PerpAlign.h
+++ Source/Project/Public/PerpAlign.h
@@ -24,7 +24,7 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-private:
+private:	
 	// Timeline components.
 	FTimeline MovementTimeLine;
 	FOnTimelineFloat InTimelineProgress;
@@ -34,7 +34,14 @@ private:
 	void InProgress();
 	void OnFinished();
 
+	// Debug Message function.
+	void DebugMessage(FString Message, FColor Color, float Delay);
+
 public:
+	// Flags.
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "PerpAlign")
+	bool IsDebug;
+	
 	// Timeline components.
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "PerpAlignTimeline")
 	UCurveFloat* CurveFloat;
