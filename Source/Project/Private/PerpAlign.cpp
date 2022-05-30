848490467fdb913a059f9fe625768c6f388dfdc6
KiSae-Lee <kisaelee.github@gmail.com>
2022-05-30 17:47:08 +0900

    initiate isdebug.

--- Source/Project/Private/PerpAlign.cpp
+++ Source/Project/Private/PerpAlign.cpp
@@ -8,7 +8,8 @@ APerpAlign::APerpAlign()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	
+
+	IsDebug = true;
 }
 
 // Called when the game starts or when spawned
